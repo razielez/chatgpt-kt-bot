792f807aa7dc4ae3a589c793f6ba346e22e4d365
razielez <linran@live.com>
2023-03-12 17:45:46 +0800

    chore:.

--- app/src/main/kotlin/chatgpt/kt/bot/app/common/SlackEventPublisher.kt
+++ app/src/main/kotlin/chatgpt/kt/bot/app/common/SlackEventPublisher.kt
@@ -1,13 +1,20 @@
 package chatgpt.kt.bot.app.common
 
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 
 open class SlackEventPublisher(
     private val channel: Channel<SlackEvent>
 ) : EventPublisher<SlackEvent> {
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun send(event: SlackEvent) {
-
+        GlobalScope.launch {
+            kotlin.run {
+                channel.send(event)
+            }
+        }
     }
-
 }
--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-unzip -o app.zip
-app_path=./app-0.1.0
-ITEM_NAME=app
-killall java
-nohup ${app_path}/bin/app start &
+URL=$(curl -s https://api.github.com/repos/razielez/chatgpt-kt-bot/releases/latest | jq '.assets | .[] | .browser_download_url' | grep jar | cut -d'"' -f2 | head -1)
+echo "url: $URL"
+JAR=chatgpt-kt-bot.jar
+wget "$URL"
+nohup java -jar -Xmx10m $JAR &
+
