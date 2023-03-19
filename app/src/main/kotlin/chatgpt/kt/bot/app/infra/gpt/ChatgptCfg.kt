dfaaa556099d7c655f4ba95b990f15eb540a8e9c
razielez <linran@live.com>
2023-03-19 22:13:41 +0800

    chore:.

--- app/src/main/kotlin/chatgpt/kt/bot/app/infra/gpt/ChatgptCfg.kt
+++ app/src/main/kotlin/chatgpt/kt/bot/app/infra/gpt/ChatgptCfg.kt
@@ -13,10 +13,10 @@ open class ChatgptCfg {
     @Bean
     open fun chatgptOkHttpClient(properties: ChatGptProperties): OkHttpClient {
         val b = OkHttpClient().newBuilder()
-            .callTimeout(Duration.ofMinutes(3))
-            .readTimeout(Duration.ofMinutes(3))
-            .writeTimeout(Duration.ofMinutes(3))
-            .connectTimeout(Duration.ofMinutes(3))
+            .callTimeout(Duration.ofMinutes(2))
+            .readTimeout(Duration.ofMinutes(2))
+            .writeTimeout(Duration.ofMinutes(2))
+            .connectTimeout(Duration.ofMinutes(2))
         properties.proxyAddr?.also {
             val proxy = it.split(":")
             b.proxy(Proxy(Proxy.Type.SOCKS, InetSocketAddress(proxy[0], proxy[1].toInt())))
